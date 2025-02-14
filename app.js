19d21a2797a94335fb1956c00d539db76530cfd8
Anna Júlia Gomes <annajuliaggomes@gmail.com>
2025-02-14 14:53:45 -0300

    app.js
    Linguagem utilizada

---
+++ app.js
@@ -0,0 +1,63 @@
+// variavel do nome
+let nome = "";
+
+//criando função de salvar nome
+function salvarNome() {
+  nome = document.getElementById("nome").value;
+  if (nome == "") {
+    alert("Por favor, insira seu nome");
+  } else {
+    listaDeAmigos.push(nome);
+    document.getElementById("nome").value = "";
+    atualizarLista();
+  }
+}
+
+// criando lista de amigos vazia
+let listaDeAmigos = [];
+
+//criando função de adicionar amigo na lista
+function adicionarAmigo() {
+  let nomeAmigo = document.getElementById("amigo").value;
+  if (nomeAmigo == "") {
+    alert("Por favor, insira um nome");
+  } else {
+    listaDeAmigos.push(nomeAmigo);
+    document.getElementById("amigo").value = "";
+    atualizarLista();
+  }
+}
+// funcão para atualizar a lista de amigos
+function atualizarLista() {
+  let listaAtualizada = document.getElementById("listaAmigos");
+  listaAtualizada.innerHTML = "";
+  for (let i = 0; i < listaDeAmigos.length; i++) {
+    let itemLista = document.createElement("li");
+    itemLista.innerHTML = listaDeAmigos[i];
+    listaAtualizada.appendChild(itemLista);
+  }
+}
+// funcão para sortear amigo e também impedir que a pessoa tire seu proprio nome
+function sortearAmigo() {
+  if (listaDeAmigos.length == 0) {
+    alert("Por favor, insira um nome.");
+    return;
+
+  }
+  let indiceAleatorio = Math.floor(Math.random() * listaDeAmigos.length);
+  let nomeSorteado = listaDeAmigos[indiceAleatorio];
+  if (nomeSorteado == nome) {
+    alert("Você não pode ser seu próprio amigo secreto! Por favor, tente novamente.");
+    return;
+  }
+
+  document.getElementById("resultado").innerHTML = `Seu amigo secreto é: ${nomeSorteado}`;
+
+}
+
+function reiniciarSorteio() {
+  document.getElementById("listaAmigos").innerHTML = ""; // Limpa a lista de amigos
+  document.getElementById("resultado").innerHTML = ""; // Limpa os resultados
+  alert("O sorteio foi reiniciado! Vamos jogar novamente. Adicione novos amigos.");
+}
+
